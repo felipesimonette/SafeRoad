85bd73bb51389068b2f4c21dfad8c9e515be0a6d
vinicius freitas <vini.freirefreitas00@gmail.com>
2020-10-21 22:02:30 -0300

    selection list

--- src/pages/chamados/index.js
+++ src/pages/chamados/index.js
@@ -8,6 +8,8 @@ import {
 } from 'react-native';
 import MapView, {Marker} from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+import Icon from 'react-native-vector-icons/Feather';
+//  import {Dropdown} from 'react-native-material-dropdown';
 
 import style from './style';
 
@@ -17,6 +19,18 @@ export default function Menu() {
   const [userPositonAvailiable, setUserPositonAvailiable] = useState(false);
   const [latitude, setLatitude] = useState(0);
   const [longitude, setLongitude] = useState(0);
+
+  // let data = [
+  //   {
+  //     value: 'Banana',
+  //   },
+  //   {
+  //     value: 'Mango',
+  //   },
+  //   {
+  //     value: 'Pear',
+  //   },
+  // ];
 
   async function getUserPermission() {
     try {
@@ -38,17 +52,6 @@ export default function Menu() {
 
   useEffect(() => {
     getUserPermission();
-
-    // Geolocation.getCurrentPosition(
-    //   (position) => {
-    //     console.log(position);
-    //   },
-    //   (error) => {
-    //     // See error code charts below.
-    //     console.log(error.code, error.message);
-    //   },
-    //   {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-    // );
 
     if (hasLocationPermission) {
       Geolocation.getCurrentPosition(
@@ -72,17 +75,41 @@ export default function Menu() {
   return (
     <View style={{flex: 1}}>
       {userPositonAvailiable ? (
-        <MapView
-          style={{flex: 1}}
-          initialRegion={{
-            latitude: latitude,
-            longitude: longitude,
-            latitudeDelta: 0.0888,
-            longitudeDelta: 0.08888,
-          }}>
-          <Marker
-            coordinate={{latitude: latitude, longitude: longitude}}></Marker>
-        </MapView>
+        <>
+          <MapView
+            style={{flex: 1}}
+            initialRegion={{
+              latitude: latitude,
+              longitude: longitude,
+              latitudeDelta: 0.0888,
+              longitudeDelta: 0.08888,
+            }}>
+            <Marker
+              coordinate={{latitude: latitude, longitude: longitude}}
+              title="Sua localização"></Marker>
+          </MapView>
+          <View
+            style={{
+              position: 'absolute',
+              backgroundColor: '#dadada',
+              right: 10,
+              left: 10,
+              bottom: 10,
+              borderRadius: 5,
+              elevation: 5,
+            }}>
+            {/* <Dropdown label="Favorite Fruit" data={data} /> */}
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#FF8C00',
+                alignSelf: 'flex-end',
+                borderRadius: 5,
+                // elevation: 5,
+              }}>
+              <Icon name="plus" color="#fff" size={45} />
+            </TouchableOpacity>
+          </View>
+        </>
       ) : (
         <View>
           <Text>Sem posição do usuario</Text>
